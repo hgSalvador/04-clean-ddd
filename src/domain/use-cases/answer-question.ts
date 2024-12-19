afae00404dee21da1dd77c1cea8d1e05a57111f3
HugoSalvador <hugossalvador1241@gmail.com>
2024-12-19 10:49:39 -0300

    Installed eslint and configured it

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,29 +1,29 @@
-import { UniqueEntityID } from "../../core/entities/unique.entity-id"
-import { Answer } from "../entities/answer"
-import { AnswerRepository } from "../repositories/answers-repository"
+import { UniqueEntityID } from '../../core/entities/unique.entity-id'
+import { Answer } from '../entities/answer'
+import { AnswerRepository } from '../repositories/answers-repository'
 
 interface AnswerQuestionUseCaseRequest {
-    instructorId: string
-    questionId: string
-    content: string
+  instructorId: string
+  questionId: string
+  content: string
 }
 
-
 export class AnswerQuestionUseCase {
-    constructor(
-        private answersRepository: AnswerRepository
-    ) {}
+  constructor(private answersRepository: AnswerRepository) {}
 
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+    })
 
-    async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-        const answer = Answer.create({
-            content,
-            authorId: new UniqueEntityID(instructorId),
-            questionId: new UniqueEntityID(questionId)
-        })
+    await this.answersRepository.create(answer)
 
-        await this.answersRepository.create(answer)
-
-        return answer
-    }
+    return answer
+  }
 }
