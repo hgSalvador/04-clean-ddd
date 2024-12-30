4f7e097e46defff87d934893d61087316a6be2d4
HugoSalvador <hugossalvador1241@gmail.com>
2024-12-30 12:08:47 -0300

    Created a use-case for delete question comments Created a use-case for delete answer comments Created a use-case to fetch question comments Created a use-case to fetch answer comments Created a unit test for delete question comments Created a unit test for delete answer comments Created a unit test for fetch question comments Created a unit test for fetch answer comments

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,5 +1,9 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 
 export interface AnswerCommentsRepository {
-  create(AnswerComment: AnswerComment): Promise<void>
+  findById(id: string): Promise<AnswerComment | null>
+  findManyByAnswerId(questionId: string, params: PaginationParams): Promise<AnswerComment[]>
+  create(answerComment: AnswerComment): Promise<void>
+  delete(answerComment: AnswerComment): Promise<void>
 }
