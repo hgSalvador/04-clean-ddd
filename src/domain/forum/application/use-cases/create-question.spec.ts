d649931f74b5d688e761694cb40d789d1e788a7d
HugoSalvador <hugossalvador1241@gmail.com>
2025-01-07 18:49:47 -0300

    Created a aggregate root base entity Created a attachment entity Created a answer attachment entity Created a question attachment

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "@/core/entities/unique.entity-id"
 import { CreateQuestionUseCase } from "./create-question"
 import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository"
 
@@ -16,10 +17,16 @@ describe('Create Question', () => {
     const result = await sut.execute({
       authorId: '1',
       title: 'Nova pergunta',
-      content: 'Conteudo da pergunta'
+      content: 'Conteudo da pergunta',
+      attachmentsIds: ['1', '2']
     })
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryQuestionRepository.items[0]).toEqual(result.value?.question)
+    expect(inMemoryQuestionRepository.items[0].attachments).toHaveLength(2)
+    expect(inMemoryQuestionRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
+    ])
   })
 })
