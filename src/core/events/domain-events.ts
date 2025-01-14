0daf73785ea86d79025a4fe23b81507832f02acb
HugoSalvador <hugossalvador1241@gmail.com>
2025-01-14 07:41:06 -0300

    Created generic unit test generic to validated a domain event class
    Created a AnswerCreatedDomain to register a event
    when a answer is created

--- src/core/events/domain-events.ts
+++ src/core/events/domain-events.ts
@@ -1,31 +1,39 @@
 import { AggregateRoot } from '../entities/aggregate-root'
 import { UniqueEntityID } from '../entities/unique.entity-id'
 import { DomainEvent } from './domain-event'
+
 type DomainEventCallback = (event: any) => void
 export class DomainEvents {
   private static handlersMap: Record<string, DomainEventCallback[]> = {}
   private static markedAggregates: AggregateRoot<any>[] = []
   
+
   public static markAggregateForDispatch(aggregate: AggregateRoot<any>) {
     const aggregateFound = !!this.findMarkedAggregateByID(aggregate.id)
     if (!aggregateFound) {
       this.markedAggregates.push(aggregate)
     }
   }
+
   private static dispatchAggregateEvents(aggregate: AggregateRoot<any>) {
     aggregate.domainEvents.forEach((event: DomainEvent) => this.dispatch(event))
   }
+
   private static removeAggregateFromMarkedDispatchList(
     aggregate: AggregateRoot<any>,
   ) {
     const index = this.markedAggregates.findIndex((a) => a.equals(aggregate))
     this.markedAggregates.splice(index, 1)
   }
+
+
   private static findMarkedAggregateByID(
     id: UniqueEntityID,
   ): AggregateRoot<any> | undefined {
     return this.markedAggregates.find((aggregate) => aggregate.id.equals(id))
   }
+
+
   public static dispatchEventsForAggregate(id: UniqueEntityID) {
     const aggregate = this.findMarkedAggregateByID(id)
     if (aggregate) {
@@ -34,6 +42,7 @@ export class DomainEvents {
       this.removeAggregateFromMarkedDispatchList(aggregate)
     }
   }
+  
   public static register(
     callback: DomainEventCallback,
     eventClassName: string,
